82935db5fb8a2955a357bd38208b7d9ec3e6498b
Gustavo Silvera <grs5382@gmail.com>
2023-04-05 18:38:52 -0400

    adding torch device and train/val split

--- mini-gpt.py
+++ mini-gpt.py
@@ -3,6 +3,15 @@ import numpy as np
 import torch
 import os
 
+device = torch.device("cpu")
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print("Using Apple M1 Metal GPU acceleration!")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using NVidia Cuda acceleration!")
+print()
+
 # Karpathy's tiny-shakespeare
 dataset = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
 filename = "input.txt"  # name of final text dataset
@@ -25,7 +34,7 @@ vocabulary_size: int = len(characters)
 print(f"Available characters ({vocabulary_size}) in dataset: {characters}")
 print()
 
-# create encoder/decoder for mapping between int <-> char
+# create encoder/decoder (character-level tokenizer) for mapping between int <-> char
 stoi: Dict[str, int] = {ch: i for i, ch in enumerate(characters)}
 itos: Dict[int, str] = {i: ch for ch, i in stoi.items()}
 assert len(stoi) == len(itos)
@@ -43,3 +52,17 @@ def decoder(ints: List[int]) -> str:
 
 print("Encoder test: ", encoder("Hello There!"))
 print("Decoder test: ", decoder(encoder("Hello There!")))
+print()
+
+# encoding the entire dataset and using Torch:
+dataset_t: torch.Tensor = torch.tensor(
+    encoder(dataset), dtype=torch.int8, device=device
+)
+print(f"Loaded entire dataset into torch Tensor: {dataset_t.shape}, {dataset_t.dtype}")
+
+# split dataset into training and validation
+percentage_for_training: float = 0.9  # % used for training, remaining used for val
+cutoff: int = int(len(dataset_t) * percentage_for_training)
+train: torch.Tensor = dataset_t[:cutoff]
+val: torch.Tensor = dataset_t[cutoff:]
+print(f"Split dataset into training: {train.shape} and val: {val.shape}")
