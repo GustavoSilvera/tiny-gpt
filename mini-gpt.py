8d131487e4671f530991f26914129509876aaa4f
Gustavo Silvera <grs5382@gmail.com>
2023-04-05 18:29:16 -0400

    adding encoder/decoder from char<->int

--- mini-gpt.py
+++ mini-gpt.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 import numpy as np
 import torch
 import os
@@ -15,13 +16,30 @@ assert os.path.exists(filename)
 print(f'Using text dataset from "{filename}"')
 
 dataset = ""
-with open(filename, 'r') as f:
+with open(filename, "r") as f:
     dataset = f.read()
 
 # print metadata about this dataset
-characters = sorted(list(set(dataset))) # get the ordered unique characters in the text
+characters = sorted(list(set(dataset)))  # get the ordered unique characters in the text
 vocabulary_size: int = len(characters)
 print(f"Available characters ({vocabulary_size}) in dataset: {characters}")
 print()
 
+# create encoder/decoder for mapping between int <-> char
+stoi: Dict[str, int] = {ch: i for i, ch in enumerate(characters)}
+itos: Dict[int, str] = {i: ch for ch, i in stoi.items()}
+assert len(stoi) == len(itos)
 
+
+def encoder(s: str) -> List[int]:
+    # given a string, return its encoding as a per-character list of integers
+    return [stoi[c] for c in s]
+
+
+def decoder(ints: List[int]) -> str:
+    # inverse of encoder, take a list of integers and create its corresponding str
+    return "".join([itos[i] for i in ints])
+
+
+print("Encoder test: ", encoder("Hello There!"))
+print("Decoder test: ", decoder(encoder("Hello There!")))
