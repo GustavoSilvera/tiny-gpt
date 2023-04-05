78f9328b5f020ccb89af7a790fafa8cc29f11d94
Gustavo Silvera <grs5382@gmail.com>
2023-04-05 18:23:33 -0400

    adding dataset and initial parser

--- mini-gpt.py
+++ mini-gpt.py
@@ -1,3 +1,27 @@
 import numpy as np
 import torch
+import os
+
+# Karpathy's tiny-shakespeare
+dataset = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+filename = "input.txt"  # name of final text dataset
+
+if not os.path.exists(filename):
+    print(f'Downloading text-dataset from "{dataset}"')
+    print()
+    os.system(f"wget {dataset}")
+
+assert os.path.exists(filename)
+print(f'Using text dataset from "{filename}"')
+
+dataset = ""
+with open(filename, 'r') as f:
+    dataset = f.read()
+
+# print metadata about this dataset
+characters = sorted(list(set(dataset))) # get the ordered unique characters in the text
+vocabulary_size: int = len(characters)
+print(f"Available characters ({vocabulary_size}) in dataset: {characters}")
+print()
+
 
